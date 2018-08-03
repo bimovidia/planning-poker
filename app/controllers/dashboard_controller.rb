ac2f5f9c78f3aa005dad5ecc0a3ebe66d2f99787
Adnan Hemani <adnan.h@berkeley.edu>
2018-08-02 23:46:12 -0700

    solve issue9 with hacky fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -40,12 +40,17 @@ class DashboardController < ApplicationController
 
   def index
     @projects = @client.projects
+    if session[:last_project]
+      curr_proj = @client.project(session[:last_project])
+      @projects.unshift @projects.delete(curr_proj)
+    end
+    session[:last_project] = @projects.first.id
   end
 
   # Ajax
   def project
     @project = @client.project(params[:id].to_i)
-
+    session[:last_project] = params[:id].to_i
     respond_with @project do |format|
       format.js { render 'dashboard/ajax/project' }
     end

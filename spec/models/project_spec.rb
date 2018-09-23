72a86c8b7fd653a925b056815dd21f7cefd9bc93
Adnan Hemani <adnan.h@berkeley.edu>
2018-09-23 03:08:31 -0700

    added tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -22,4 +22,33 @@ describe Project, type: :model do
             assert_equal Project.all.first.pivotal_id, project_id
         end
     end
+
+    context "#get_milestones" do
+        let(:project) { mock('project') }
+        before do
+            story1 = mock("story1")
+            story2 = mock("story2")
+            story3 = mock("story3")
+            story4 = mock("story4")
+            story5 = mock("story5")
+            story1.stubs(:name).returns("story1")
+            story2.stubs(:name).returns("story2")
+            story3.stubs(:name).returns("story3")
+            story4.stubs(:name).returns("story4")
+            story5.stubs(:name).returns("story5")
+            story1.stubs(:deadline).returns("deadline1")
+            story2.stubs(:deadline).returns("deadline2")
+            story3.stubs(:deadline).returns("deadline3")
+            project.stubs(:stories).with(has_entry(:filter, 'story_type:release current_state:unscheduled')).returns([story4, story5])
+            project.stubs(:stories).with(has_entry(:filter, 'story_type:release current_state:unstarted')).returns([story1, story2, story3])
+        end
+        it "returns a string" do
+            retVal = Project.get_milestones(project)
+            expect(retVal.is_a?(String)).to be true
+        end
+        it "returns milestones in a certain order and format" do
+            retVal = Project.get_milestones(project)
+            assert_equal retVal, "story1: deadline1\nstory2: deadline2\nstory3: deadline3\nstory4\nstory5"
+        end
+    end
 end
